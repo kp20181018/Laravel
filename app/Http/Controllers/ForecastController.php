f9cd4a4940f1f804152fe61f3be5919d7cd7eb2b
kp20181018 <kp20181018@student.fon.bg.ac.rs>
2022-01-14 02:29:49 +0100

    Kreiran kontroler za rad sa prognozom

---
+++ app/Http/Controllers/ForecastController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Forecast;
+use Illuminate\Http\Request;
+
+class ForecastController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Forecast::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $forecast = Forecast::create($request->all());
+        response()->json(Forecast::find($forecast->id));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Forecast $forecast)
+    {
+        response()->json($forecast);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Forecast $forecast)
+    {
+        $forecast->update($request->all());
+        response()->json($forecast);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Forecast $forecast)
+    {
+        $forecast->delete();
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ForecastController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,5 @@ Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->apiResource('forecasts', ForecastController::class);
